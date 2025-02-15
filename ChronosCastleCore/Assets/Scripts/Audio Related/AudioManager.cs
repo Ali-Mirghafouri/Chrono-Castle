8cf65006216da01903718f12d738285593849963
JackS <pavenkalaichelvam2002@gmail.com>
2025-02-15 19:44:50 +0800

    Added lean tween library

---
+++ ChronosCastleCore/Assets/Scripts/Audio Related/AudioManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager current;
+    private void Awake()
+    {
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
